7b4405c4aedc5749f53eba934290a11a04a9e950
Lucas Matheus Nunes Pereira <Lucasmathues416@outlook.com>
2022-09-24 16:42:16 -0300

    App done

--- src/components/UserFooter/UserFooter.styles.ts
+++ src/components/UserFooter/UserFooter.styles.ts
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
 const UFooter = styled.footer`
-  grid-area: "footer";
+  width: 100%;
+
   display: grid;
   grid-template-columns: 1fr 1fr;
-  gap: 1rem;
-  width: 100%;
+  gap: 1.2rem;
+
   color: ${(props) => props.theme.colors.wrd400};
 
   .disabled {
@@ -14,23 +15,29 @@ const UFooter = styled.footer`
       color: ${(props) => props.theme.colors.wrd600} !important;
     }
   }
+
+  @media screen and (max-width: 500px) {
+    grid-template-columns: 1fr;
+  }
 `;
 
 const UFooterItem = styled.div`
   a {
-    display: flex;
-    align-items: center;
+    text-decoration: none;
+    color: inherit;
+    display: grid;
+    grid-template-columns: auto 1fr;
     gap: 1rem;
-    text-overflow: ellipsis;
+    align-items: center;
 
     &:hover {
       cursor: pointer;
       color: ${(props) => props.theme.colors.acc900};
     }
 
-    & > * {
+    & > p {
       overflow: hidden;
-      max-width: 16ch;
+      max-width: 20ch;
       text-overflow: ellipsis;
       word-wrap: wrap;
     }
